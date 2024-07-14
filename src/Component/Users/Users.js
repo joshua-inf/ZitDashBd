8701eda40815983686365915e0fb8f26fe3fad56
Joshua Sibanda <jsibanda407@gmail.com>
2024-07-14 02:49:12 +0200

    added proxy

--- src/Component/Users/Users.js
+++ src/Component/Users/Users.js
@@ -12,7 +12,7 @@ const Users  = () => {
 
     const getdata = () => {
                 setLoading(true)        
-                axios.get(process.env.REACT_APP_ENDPOINT_USED+'/admin/users')
+                axios.get('/admin/users')
                 .then(res => {
                  setUsers(res.data.users)
                  console.log(res.data.users.includes(e => e == ''))
